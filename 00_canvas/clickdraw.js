beb584323687d95bd296cfd2403bb4ec81682155
blu4 <blu4@stuy.edu>
2019-01-31 07:28:54 -0500

    donezo

--- 00_canvas/clickdraw.js
+++ 00_canvas/clickdraw.js
@@ -1,17 +1,61 @@
-document.getElementById("clear").addEventListener("click", clearCanvas);
+// Bo Hui Lu
+// SoftDev pd7
+// K00 -- I See a Red Door
+// 2019-01-30
+
+
+var clr = document.getElementById("clear");
+var tog = document.getElementById("toggle");
+
+var mode = true; //rect = truee, dot = false
+
 
 var c = document.getElementById("slate");
 var ctx = c.getContext("2d");
+ctx.fillStyle= 'red';
+
+clr.addEventListener("click", clearCanvas );
+tog.addEventListener("click", newMode );
+
+tog.innerHTML = "Click if ye want dots";
 
 function clearCanvas() {
-    ctx.clearRect(0, 0, 300, 300);
+    ctx.fillStyle = "white";
+    ctx.clearRect(0, 0, 500, 500);
+    ctx.fillStyle = "red";
+
+}
+
+function newMode() {
+    if (mode) {
+        mode = false;
+        tog.innerHTML = "Click if ye want rectangles"
+    }
+    else{
+        mode = true;
+        tog.innerHTML = "Click if ye want dots"
+    }
+
+}
+
+function drawStuff(e) {
+    if (mode == true) { //rectt mode
+        ctx.fillRect(e.offsetX - 50, e.offsetY - 75, 50, 100)
+
+    }
+    else{
+        ctx.beginPath()
+        ctx.ellipse(e.offsetX, e.offsetY, 5, 5, 0, Math.PI, Math.PI*3)
+        ctx.fill();
+        ctx.stroke();
+
+    }
+
 
 }
 
 
-document.getElementById("slate").addEventListener("click", drawRect);
+c.addEventListener("click", drawStuff);
 
-function drawRect() {
-    ctx.fillRect(0, 0, 10, 15);
-
-}
+console.log(c);
+console.log(ctx);
