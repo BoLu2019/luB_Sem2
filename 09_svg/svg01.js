d628a9f61f0b0b4b8fbf5fb1e44eb04e4e84f05b
BoLu2019 <blu4@stuy.edu>
2019-03-13 13:18:50 -0400

    fixed some issues

--- 09_svg/svg01.js
+++ 09_svg/svg01.js
@@ -7,43 +7,48 @@
 var pict = document.getElementById("vimage");
 var button = document.getElementById("but_clear");
 
-var firX = 1; //first x
-var firY = 0; //first y
+var firX = null; //first x
+var firY = null; //first y
 
-var clear = (e) => {
+var clear = function(e) {
     while (pict.firstChild) {
         pict.removeChild(pict.firstChild);
     }
-    firX = 1;
-    firY = 0;
+    firX = null;
+    firY = null;
 }
 
-var draw = (e) => {
-    var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle"); 
-    var line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+var draw = function(e) {
+   
 
     //draws line when not the first pt, cuz the first one would be a circle
-    if( firX != 1){
+    if( firX != null){
+	e.preventDefault();
+	var line = document.createElementNS("http://www.w3.org/2000/svg", "line");
         line.setAttribute('x1',firX);
         line.setAttribute('y1',firY);
         line.setAttribute('x2',e.offsetX);
         line.setAttribute('y2',e.offsetY);
         line.setAttribute("stroke", "black")
+	pict.appendChild(line);
+	
     }  
     
     //circle
-    circle.setAttribute("cx",e.offSetX);
-    circle.setAttribute("cy",e.offSetY);
+    var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle"); 
+    circle.setAttribute("cx",e.offsetX);
+    circle.setAttribute("cy",e.offsetY);
     circle.setAttribute("r", 15);
     circle.setAttribute("fill","purple");
     circle.setAttribute("stroke","black");
     //Add to pict
     pict.appendChild(circle);
-    pict.appendChild(line);
+
+    
     //Update vars
     firX = e.offSetX;
     firY = e.offSetY;
-
+    
 
 }
 
