3be66186f476785b5b245f379894208c2ed33aad
blu4 <blu4@stuy.edu>
2019-03-20 23:40:07 -0400

    fin

---
+++ 14_svg/d3.js
@@ -0,0 +1,16 @@
+// Bohui Lu
+// SoftDev2 pd7
+// K14 -- Learning To Swim
+// 2019-03-20
+
+var selection = d3.select("body");
+console.log(selection);
+
+var head = d3.selectAll("h1");
+console.log(head);
+
+var sect = d3.selectALl("section");
+console.log(sect);
+
+var sectApp = sect.append("p");
+console.log(sectApp);
